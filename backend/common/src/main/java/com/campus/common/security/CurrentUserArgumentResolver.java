b51060fc9e92a42ef89c96a7fe2e52a898c3e076
AuthenticMan <2703150761@qq.com>
2026-05-23 20:00:37 +0800

    测试性修改

--- backend/common/src/main/java/com/campus/common/security/CurrentUserArgumentResolver.java
+++ backend/common/src/main/java/com/campus/common/security/CurrentUserArgumentResolver.java
@@ -10,6 +10,8 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+
+
 public class CurrentUserArgumentResolver implements HandlerMethodArgumentResolver {
 
     @Override
